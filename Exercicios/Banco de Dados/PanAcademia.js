9f5bfcec3658d8bbbd4cd4974f2ff2b829515c69
MedeirosRodrigo <rodrigo.medeiros30@gmail.com>
2021-11-03 19:27:40 -0300

    Subindo Bando de dados

---
+++ Exercicios/Banco de Dados/PanAcademia.js
@@ -0,0 +1,119 @@
+/* 1 */
+
+{
+    "_id" : ObjectId("617d995f6154d40c4c0c56ef"),
+    "Professor" : {
+        "nome" : "Jenifer",
+        "especialidade" : "Java"
+    },
+    "Aluno" : {
+        "nome" : "Rodrigo Medeiros",
+        "telefone" : "(11) 9888-1234",
+        "Idade" : 30
+    },
+    "Modulo" : 1,
+    "AtividadesJava" : "Spring Boot",
+    "AtividadesDB" : "Criar Banco de dados",
+    "Nivel" : "Iniciante",
+    "dataHora" : ISODate("2021-07-12T10:00:20.000Z")
+}
+
+/* 2 */
+
+{
+    "_id" : ObjectId("617d9c6d0de575995e58b7c5"),
+    "Professor" : {
+        "nome" : "Jonatan",
+        "especialidade" : "Java"
+    },
+    "Aluno" : {
+        "nome" : "Wesley",
+        "telefone" : "(11) 7777-7777",
+        "Idade" : 35
+    },
+    	"Modulo" : 1,
+ 	"AtividadesJava" : "Laco de repeticao",
+        "AtividadesDB" : "Criar de banco de dados",
+        "Nivel" : "Iniciante",
+ 	 "dataHora" : ISODate("2021-07-12T10:00:00.000Z")
+}
+
+/* 3 */
+
+{
+    "_id" : ObjectId("617d9c6d0de575995e58b7c9"),
+    "Professor" : {
+        "nome" : "Ana",
+        "especialidade" : "Git / Github"
+    },
+    "Aluno" : {
+        "nome" : "Carlos",
+        "telefone" : "(11) 7777-7777",
+        "Idade" : 45
+    },
+    	"Modulo" : 2,
+ 	"AtividadesJava" : "Spring Boot",
+        "AtividadesDB" : "Criar de banco de dados",
+        "Nivel" : "Expert",
+ 	 "dataHora" : ISODate("2021-07-12T11:30:00.000Z")
+}
+
+/* 4 */
+
+{
+    "_id" : ObjectId("617d9c6d0de575995e58b710"),
+    "Professor" : {
+        "nome" : "Jenifer",
+        "especialidade" : "Java"
+    },
+    "Aluno" : {
+        "nome" : "Filipe",
+        "telefone" : "(11) 7777-7777",
+        "Idade" : 22
+    },
+    	"Modulo" : 2,
+ 	"AtividadesJava" : "Spring Boot",
+        "AtividadesDB" : "Criar de banco de dados",
+        "Nivel" : "Intermediario",
+ 	 "dataHora" : ISODate("2021-07-12T09:00:00.000Z")
+}
+
+/* 5 */
+
+{
+"_id" : ObjectId("617d9c6d0de575995e58b711"),
+"Professor" : {
+    "nome" : "Jonatan",
+    "Nivel" : "Git / Github"
+},
+"Aluno" : {
+    "nome" : "Icaro",
+    "telefone" : "(11) 7777-7777",
+    "Idade" : 20
+},
+    "Modulo" : 3,
+ "AtividadesJava" : "Spring Boot",
+    "AtividadesDB" : "Criar de banco de dados",
+    "Nivel" : "Intermediario",
+  "dataHora" : ISODate("2021-07-12T12:00:00.000Z")
+}
+
+/* 6 */
+
+{
+    "_id" : ObjectId("617d9c6d0de575995e58b712"),
+    "Professor" : {
+        "nome" : "Ana",
+        "especialidade" : "Git / Github"
+    },
+    "Aluno" : {
+        "nome" : "Mateus",
+        "telefone" : "(11) 7777-7777",
+        "Idade" : 29
+    },
+    	"Modulo" : 3,
+ 	"AtividadesJava" : "Spring Boot",
+        "AtividadesDB" : "Criar de banco de dados",
+        "Nivel" : "Intermediario",
+ 	 "dataHora" : ISODate("2021-07-12T13:00:00.000Z")
+}
